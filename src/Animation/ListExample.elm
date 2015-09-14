009715f82891f75a00e489206fa60c0c50538abf
Sean Clark Hess <seanhess@gmail.com>
2015-09-14 14:15:27 -0600

    starting to switch to new list example

--- src/Animation/ListExample.elm
+++ src/Animation/ListExample.elm
@@ -1,8 +1,8 @@
 module Animation.ListExample where
 
-import Html exposing (div, button, text, Html, pre)
-import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
+import Html as H exposing (..)
+import Html.Attributes as A exposing (..)
+import Html.Events as E exposing (..)
 import StartApp as StartApp
 import Signal exposing (Address)
 import Animation exposing (animation, from, to, duration, delay, Animation, static, animate)
@@ -12,43 +12,6 @@ import Task exposing (Task)
 import List
 import String
 import Debug
-
---------------------------------------------------------------------
-
-type alias Transition =
-  { animations : List Animation
-  , time : Time
-  }
-
-none = { animations = [], time = 0 }
-
-add : Animation -> Transition -> Transition
-add a trans = { trans | animations <- a :: trans.animations }
-
-tick : Time -> Transition -> Transition
-tick time trans =
-  let anims = List.filter (not << Animation.isDone time) trans.animations
-  in { trans | animations <- anims
-             , time <- time
-     }
-
-value : Transition -> Float
-value trans =
-  List.sum <| List.map (animate trans.time) trans.animations
-
-dump : Transition -> String
-dump trans =
-  String.join "\n"
-    [ "time: " ++ toString (round (trans.time / 1000))
-    , "num: " ++ toString (List.length trans.animations)
-    , toString <| List.map (animate trans.time) trans.animations
-    ]
-
-anim : Time -> Float -> Animation
-anim t s =
-  animation t |> from s |> to 0 |> duration (0.8 * second)
-
---------------------------------------------------------------------
 
 type PageState = Open | Closed
 type SortOrder = SortName | SortId
@@ -61,12 +24,14 @@ type alias Item =
 type alias Model =
   { sort : SortOrder
   , items : List ItemModel
+  , newItemText : String
   }
 
 init : ( Model, Effects Action )
 init =
   ( { sort = SortId
     , items = List.indexedMap itemModel [Item 1 "one", Item 2 "two", Item 3 "three", Item 4 "four", Item 5 "five", Item 6 "six"]
+    , newItemText = ""
     }
   , Effects.tick Tick )
 
@@ -75,6 +40,8 @@ init =
 type Action
   = Sort SortOrder
   | Tick Time
+  | UpdateText String
+  | NewItem
 
 update : Action -> Model -> (Model, Effects Action)
 update action model =
@@ -87,10 +54,24 @@ update action model =
       ( { model | sort <- order, items <- items }
       , Effects.none )
 
+    UpdateText txt ->
+      ( { model | newItemText <- txt }
+      , Effects.none )
+
+    -- can't do this yet because I'm storing the list order
+    -- no good. Everything has to be transitive
+    -- NewItem ->
+      -- let item = Item (nextId model.items) model.newItemText in
+      -- ( { model | newItemText <- "", items <- item :: model.items }
+      -- , Effects.none )
+
     Tick t ->
-      let items = List.map (updateItem (ItemTick t)) model.items in
+      let items = List.map (updateTime t) model.items in
       ( { model | items <- items }
       , Effects.tick Tick )
+
+nextId : List ItemModel -> Int
+nextId mds = List.length mds + 1
 
 -----------------------------------------------------------------------------
 
@@ -103,7 +84,8 @@ sortItems order items =
 view : Address Action -> Model -> Html
 view address model =
   div [ style [("margin", "10px")] ]
-    [ div [ style [("margin-bottom", "10px")] ]
+    [ p [] [ itemInput address model ]
+    , div [ style [("margin-bottom", "10px")] ]
         [ button
             [ style (buttonStyle (model.sort == SortName))
             , onClick address (Sort SortName) ]
@@ -120,6 +102,23 @@ view address model =
         [ style [("position", "relative")] ]
         (List.map (itemView address) model.items)
     ]
+
+itemInput : Address Action -> Model -> Html
+itemInput address model =
+  H.form
+    [ style [("marginBottom", "15px")]
+    , action "javascript:void(0)"
+    , onSubmit address NewItem
+    ]
+    [ input
+        [ type' "text"
+        , placeholder "search"
+        , A.value model.newItemText
+        , on "input" targetValue (Signal.message address << UpdateText)
+        , style [("fontSize", "16px"), ("padding", "4px"), ("width", "200px"), ("marginBottom", "6px")]
+        ] []
+    ]
+
 
 
 -------------------------------------------------------
@@ -142,13 +141,9 @@ updatePosition pos model =
     , transition <- add (anim model.transition.time dy) model.transition
   }
 
-type ItemAction = ItemTick Time
-
-updateItem : ItemAction -> ItemModel -> ItemModel
-updateItem action model =
-  case action of
-    ItemTick time ->
-      { model | transition <- tick time model.transition }
+updateTime : Time -> ItemModel -> ItemModel
+updateTime time model =
+  { model | transition <- tick time model.transition }
 
 itemY : Int -> Float
 itemY pos = toFloat pos * 50.0
@@ -157,24 +152,36 @@ itemView : Address Action -> ItemModel -> Html
 itemView address model =
   let y = itemY model.position + value model.transition in
   div
-    [ style
-        [ ("background", "#E6E6EF")
-        , ("border", "solid 1px #716DCF")
-        , ("borderRightWidth", "10px")
-        , ("borderLeftWidth", "0px")
-        , ("borderTopWidth", "0px")
-        , ("margin", "10px")
-        , ("marginLeft", "0px")
-        , ("padding", "10px")
-        , ("width", "320px")
-        , ("position", "absolute")
-        , ("top", toString y ++ "px")
-        ]
-    ]
+    [ style (itemStyle y) ]
     [ text model.item.name ]
+
 
 maxWidth = 200.0
 
+-------------------------------------------------
+
+type alias Style = List (String, String)
+
+inputStyle : Style
+inputStyle =
+  [ ("fontSize", "18px")
+  , ("padding", "4px" )
+  ]
+
+itemStyle : Float -> Style
+itemStyle y =
+  [ ("background", "#E6E6EF")
+  , ("border", "solid 1px #716DCF")
+  , ("borderRightWidth", "10px")
+  , ("borderLeftWidth", "0px")
+  , ("borderTopWidth", "0px")
+  , ("margin", "10px")
+  , ("marginLeft", "0px")
+  , ("padding", "10px")
+  , ("width", "320px")
+  , ("position", "absolute")
+  , ("top", toString y ++ "px")
+  ]
 
 buttonStyle isSelected =
   let highlight =
@@ -211,3 +218,43 @@ port tasks : Signal (Task Never ())
 port tasks =
   app.tasks
 
+
+
+
+--------------------------------------------------------------------
+-- transition library
+
+type alias Transition =
+  { animations : List Animation
+  , time : Time
+  }
+
+none = { animations = [], time = 0 }
+
+add : Animation -> Transition -> Transition
+add a trans = { trans | animations <- a :: trans.animations }
+
+tick : Time -> Transition -> Transition
+tick time trans =
+  let anims = List.filter (not << Animation.isDone time) trans.animations
+  in { trans | animations <- anims
+             , time <- time
+     }
+
+value : Transition -> Float
+value trans =
+  List.sum <| List.map (animate trans.time) trans.animations
+
+dump : Transition -> String
+dump trans =
+  String.join "\n"
+    [ "time: " ++ toString (round (trans.time / 1000))
+    , "num: " ++ toString (List.length trans.animations)
+    , toString <| List.map (animate trans.time) trans.animations
+    ]
+
+anim : Time -> Float -> Animation
+anim t s =
+  animation t |> from s |> to 0 |> duration (0.8 * second)
+
+--------------------------------------------------------------------
